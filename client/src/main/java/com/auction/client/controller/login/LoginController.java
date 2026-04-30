9c8ba9f8b0125fce13df5ab871b0e7c898af5257
ADMIN <lekhanh10042007@gmail.com>
2026-04-30 21:13:27 +0700

    khanh beo cho 2 class controller vao package rieng

--- client/src/main/java/com/auction/client/controller/login/LoginController.java
+++ client/src/main/java/com/auction/client/controller/login/LoginController.java
@@ -1,4 +1,4 @@
-package com.auction.client.controller;
+package com.auction.client.controller.login;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,7 +7,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
--- client/src/main/java/com/auction/client/controller/register/RegisterController.java
+++ client/src/main/java/com/auction/client/controller/register/RegisterController.java
@@ -1,4 +1,4 @@
-package com.auction.client.controller;
+package com.auction.client.controller.register;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -6,10 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField; // Dùng PasswordField cho mật khẩu
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class RegisterController {
