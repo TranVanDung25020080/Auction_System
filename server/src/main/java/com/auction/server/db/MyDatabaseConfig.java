b67b51a9b227e5a0a718d8037adfb1ca733af2d9
ADMIN <lekhanh10042007@gmail.com>
2026-04-30 21:05:56 +0700

    khanh beo tu them class databaseconfig cua minh

---
+++ server/src/main/java/com/auction/server/db/MyDatabaseConfig.java
@@ -0,0 +1,17 @@
+package com.auction.server.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDatabaseConfig {
+    private static Connection connection;
+    private static final String URL="jdbc:mysql://localhost:3306/auctions";
+    private static final String user="root";
+    private static final String password="";
+
+    public Connection getConnection() throws SQLException {
+        connection= DriverManager.getConnection(URL,user,password);
+        return connection;
+    }
+}
