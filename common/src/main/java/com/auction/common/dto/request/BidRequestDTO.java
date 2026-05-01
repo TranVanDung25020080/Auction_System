75e60efb7f74d9bb1a6107b55bfba7ee08362e8e
ADMIN <lekhanh10042007@gmail.com>
2026-05-01 10:07:12 +0700

    them service normalbid

--- common/src/main/java/com/auction/common/dto/request/BidRequestDTO.java
+++ common/src/main/java/com/auction/common/dto/request/BidRequestDTO.java
@@ -1,20 +1,23 @@
 package com.auction.common.dto.request;
 
 public class BidRequestDTO {
-    private int auctionId;
+    private int auctionId,bidderId;
     private double bidAmount;
     private double highCurrentPrice;
 
 
-    public BidRequestDTO(int auctionId, double bidAmount,double highCurrentPrice) {
+    public BidRequestDTO(int bidderId ,int auctionId, double bidAmount,double highCurrentPrice) {
         this.auctionId = auctionId;
         this.bidAmount = bidAmount;
         this.highCurrentPrice=highCurrentPrice;
+        this.bidderId=bidderId;
     }
 
     //Getter
     //region
+    public int getBidderId(){return this.bidderId;}
     public int getAuctionId() {return auctionId;}
     public double getBidAmount() {return bidAmount;}
+    public double getHighCurrentPrice(){return this.highCurrentPrice;}
     //endregion
 }
--- server/src/main/java/com/auction/server/service/auction/BidService.java
+++ server/src/main/java/com/auction/server/service/auction/BidService.java
@@ -2,19 +2,24 @@ package com.auction.server.service.auction;
 
 import com.auction.common.dto.request.BidRequestDTO;
 import com.auction.common.dto.response.BidUpdateResponseDTO;
+import com.auction.common.enums.BidStatus;
 
 public class BidService {
     public BidUpdateResponseDTO normalBid(BidRequestDTO bidRequestDTO){
         BidUpdateResponseDTO bidUpdateResponseDTO=new BidUpdateResponseDTO();
 
+        int bidderId=bidRequestDTO.getBidderId();
         int auctionId=bidRequestDTO.getAuctionId();
         double bidAmmount=bidRequestDTO.getBidAmount();
+        double highCurrentPrice=bidRequestDTO.getHighCurrentPrice();
 
-
-
-
-
-
+        if (bidAmmount<=highCurrentPrice){
+            bidUpdateResponseDTO.setBidStatus(BidStatus.FAILED);
+        }
+        else {
+            bidUpdateResponseDTO=new BidUpdateResponseDTO(auctionId,bidderId,highCurrentPrice);
+            bidUpdateResponseDTO.setBidStatus(BidStatus.SUCCESS);
+        }
 
 
         return bidUpdateResponseDTO;
