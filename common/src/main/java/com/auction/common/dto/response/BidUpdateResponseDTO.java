df4975c86d4b52b8073de36dbed4f756bd1dac8b
ADMIN <lekhanh10042007@gmail.com>
2026-05-01 09:59:32 +0700

    them thuoc tinh bidstatus cho class BidUpdateResponseDTO

--- common/src/main/java/com/auction/common/dto/response/BidUpdateResponseDTO.java
+++ common/src/main/java/com/auction/common/dto/response/BidUpdateResponseDTO.java
@@ -1,10 +1,13 @@
 package com.auction.common.dto.response;
 
+import com.auction.common.enums.BidStatus;
+
 public class BidUpdateResponseDTO {
     private int auctionId,bidderId;
     private double newHighestPrice;
     private String highestBidderName;
     private String timeStamp;
+    private BidStatus bidStatus;
 
     //Constructor
     public BidUpdateResponseDTO(int auctionId, double newHighestPrice, String highestBidderName, String timeStamp) {
@@ -28,4 +31,9 @@ public class BidUpdateResponseDTO {
     public String getHighestBidderName() { return highestBidderName; }
     public String getTimeStamp() { return timeStamp; }
     //endregion
+
+    //setter
+    public void setBidStatus(BidStatus bidStatus){
+        this.bidStatus=bidStatus;
+    }
 }
