21eae56b3bbbbb0014078e639f4d5864640858d1
ADMIN <lekhanh10042007@gmail.com>
2026-05-12 11:35:37 +0700

    viết api cho service getItemBySellerId

---
+++ server/src/main/java/com/auction/server/handler/httpserver/GetItemHandler.java
@@ -0,0 +1,36 @@
+package com.auction.server.handler.httpserver;
+
+import com.auction.common.dto.request.GetItemRequestDTO;
+import com.auction.common.dto.response.GetItemReponseDTO;
+import com.auction.common.enums.AuthStatus;
+import com.auction.server.exception.DatabaseException;
+import com.auction.server.handler.HttpBaseHandler;
+import com.auction.server.service.item.ItemService;
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+public class GetItemHandler extends HttpBaseHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        Gson gson=new Gson();
+
+        String jsonRequest=super.getRequest(exchange);
+        GetItemRequestDTO getItemRequestDTO=gson.fromJson(jsonRequest, GetItemRequestDTO.class);
+
+        GetItemReponseDTO getItemReponseDTO=new GetItemReponseDTO();
+        try{
+            getItemReponseDTO=new ItemService().getItemBySellerId(getItemRequestDTO);
+            getItemReponseDTO.setStatus(AuthStatus.SUCCESS);
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+            getItemReponseDTO.setStatus(AuthStatus.SERVER_ERROR);
+            getItemReponseDTO.setMessage(e.getMessage());
+        }
+
+        this.response=gson.toJson(getItemReponseDTO);
+        super.handle(exchange);
+
+    }
+}
