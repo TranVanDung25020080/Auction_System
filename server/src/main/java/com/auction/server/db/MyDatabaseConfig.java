6436df2bba814c16a07024deed1e7305e5bd74d3
ADMIN <lekhanh10042007@gmail.com>
2026-05-16 20:00:37 +0700

    đang hoàn thiện api auction

--- server/src/main/java/com/auction/server/db/MyDatabaseConfig.java
+++ server/src/main/java/com/auction/server/db/MyDatabaseConfig.java
@@ -8,7 +8,7 @@ public class MyDatabaseConfig {
     private static Connection connection;
     private static final String URL="jdbc:mysql://localhost:3306/auctions";
     private static final String USER="root";
-    private static final String PASSWORD="";
+    private static final String PASSWORD="namkhanh1";
 
     /*    private static final String URL = "jdbc:mysql://localhost:3306/auction_db";
         private static final String USER = "root";
