3a9fb3291fefc06c26ca3b4d7ea848018f7e4fb3
TranVanDung25020080 <25020080@vnu.edu.vn>
2026-05-24 19:05:43 +0700

    sua ket noi

--- server/src/main/java/com/auction/server/db/MyDatabaseConfig.java
+++ server/src/main/java/com/auction/server/db/MyDatabaseConfig.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 
 public class MyDatabaseConfig {
     private static Connection connection;
-    private static final String URL= ConfigLoader.get("db.url");
-    private static final String USER= ConfigLoader.get("db.user");
-    private static final String PASSWORD= ConfigLoader.get("db.password");
+    private static final String URL = "jdbc:mysql://kodama.proxy.rlwy.net:11971/railway?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "xQocNoyDrzXAvHViQhaVmzdwPgnZSyoZ";
 
 //        private static final String URL = "jdbc:mysql://localhost:3306/auction_db";
 //        private static final String USER = "root";
