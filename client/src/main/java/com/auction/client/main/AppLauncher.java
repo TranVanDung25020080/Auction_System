ff9dd3df0f09b0547e443c94792256cce8e4b83e
TranVanDung25020080 <25020080@vnu.edu.vn>
2026-05-30 23:47:40 +0700

    sua ci

---
+++ client/src/main/java/com/auction/client/main/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.auction.client.main;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        // Gọi duy nhất hàm main thực sự của giao diện ở đây
+        MainApplication.main(args);
+    }
+}
